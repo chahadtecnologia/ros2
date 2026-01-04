99619bed45d98e6daa464266af3d59035c61a19d
MaGus Chahad <chahadtecnologia@gmail.com>
2026-01-03 22:55:27 -0300

    Adding Python for ROS2 studies

---
+++ Studies/magus_py_pkg/magus_robot_pub3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+ 
+class MagusRobotPub1(Node):
+    def __init__(self):
+        super().__init__("magus_robot_3")
+        self.declare_parameter("name", "Magus 3")
+        self.robot_name_ = self.get_parameter("name").value
+        self.create_publisher(String, "/magus_topic", 10)
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = "Data from " + self.robot_name_ + " to the Magus Server"
+        self.get_logger().info("Sending message from " + self.robot_name_)
+ 
+ 
+def main(args=None):
+    rclpy.init(args=args)
+    node = MagusRobotPub1()
+    rclpy.spin(node)
+    rclpy.shutdown()
+ 
+ 
+if __name__ == "__main__":
+    main()
---
+++ Studies/magus_py_pkg/magus_robot_sub3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+ 
+class MagusRobotSub3(Node):
+    def __init__(self):
+        super().__init__("magus_robot_sub3")
+        self.declare_parameter("name", "Magus 3")
+        self.robot_name_ = self.get_parameter("name").value
+        self.subscriber_ = self.create_subscription(
+            String, "/magus_topic", self.callback_chahad_news, 10)
+        self.get_logger().info("Receiving data from " + self.robot_name_)
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = "Data from " + self.robot_name_ + " to the Magus Server"
+        self.get_logger().info("Receiving data from " + self.robot_name_)
+ 
+    def callback_chahad_news(self, msg: String):
+        self.get_logger().info(msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MagusRobotSub3()
+    rclpy.spin(node)
+    rclpy.shutdown()
+ 
+ 
+if __name__ == "__main__":
+    main()
