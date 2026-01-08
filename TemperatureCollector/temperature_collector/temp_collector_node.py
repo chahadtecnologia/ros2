9932136fd9741cba4d8e2db3f309ab52d34fd133
MaGus Chahad <chahadtecnologia@gmail.com>
2026-01-07 23:32:49 -0300

    Adding new feature to collect temperatures using Open Weather API

--- TemperatureCollector/temperature_collector/temp_collector_node.py
+++ TemperatureCollector/temperature_collector/temp_collector_node.py
@@ -2,30 +2,51 @@
 import rclpy
 import requests
 from rclpy.node import Node
-from example_interfaces.msg import Float64
 from temperature_collector_interfaces.msg import TemperatureCollector
 
- 
 class TempCollectorNode(Node):
     def __init__(self):
         super().__init__("temp_collector_node")
         self.temp_collector_publisher_ = self.create_publisher(TemperatureCollector, "temp_collector_msg", 10)
         self.timer_ = self.create_timer(10.0, self.extract_temp_collector)
-        self.get_logger().info("Temperature Collector publisher has been started.")
+        self.get_logger().info("Temperature Collector publisher has been started...")
+
+    def get_weather_data(self):
+        api_key = "<API_TOKEN>"
+        cities = ["São Paulo", "Santo André", "Campinas"]
+        results = {}
+
+        for city in cities:
+            url = f"<OPEN_WEATHER_MAP_ENDPOINT>"
+            try:
+                response = requests.get(url)
+                data = response.json()
+                if response.status_code == 200:
+                    results[city] = data['main']['temp']
+                else:
+                    self.get_logger().error(f"Erro em {city}: {data.get('message')}")
+            except Exception as e:
+                self.get_logger().error(f"Erro de conexão em {city}: {e}")
+        
+        return results
 
     def extract_temp_collector(self):
-        msg = TemperatureCollector()
-        msg.temperature_campinas = 35.0
-        msg.temperature_santoandre = 27.0
-        msg.temperature_saopaulo = 18.0
-        self.temp_collector_publisher_.publish(msg) 
- 
+        temperatures = self.get_weather_data()
+        
+        if temperatures:
+            msg = TemperatureCollector()
+            msg.temperature_saopaulo = temperatures.get("São Paulo", 0.0)
+            msg.temperature_santoandre = temperatures.get("Santo André", 0.0)
+            msg.temperature_campinas = temperatures.get("Campinas", 0.0)
+            
+            self.temp_collector_publisher_.publish(msg)
+            self.get_logger().info(f"Temperature Collected: São Paulo: {msg.temperature_saopaulo} | Santo André: {msg.temperature_santoandre} | Campinas: {msg.temperature_campinas}")
+
 def main(args=None):
     rclpy.init(args=args)
     node = TempCollectorNode()
     rclpy.spin(node)
     rclpy.shutdown()
- 
- 
+
 if __name__ == "__main__":
     main()
--- TemperatureCollector/temperature_collector/temp_server_node.py
+++ TemperatureCollector/temperature_collector/temp_server_node.py
@@ -6,12 +6,12 @@ from temperature_collector_interfaces.msg import TemperatureCollector
 class TempServerNode(Node):
     def __init__(self):
         super().__init__("temp_server_node")
-        self.temp_server_subscription_ = self.create_subscription(TemperatureCollector, "temp_collector_msg", self.listener_callback, 10)
         self.current_msg_ = None
+        self.temp_server_subscription_ = self.create_subscription(TemperatureCollector, "temp_collector_msg", self.listener_callback, 10)
         self.timer_ = self.create_timer(10.0, self.callback_temp_server)
 
-        self.declare_parameter("city1", "Sao Paulo")
-        self.declare_parameter("city2", "Santo Andre")
+        self.declare_parameter("city1", "São Paulo")
+        self.declare_parameter("city2", "Santo André")
         self.declare_parameter("city3", "Campinas")
 
         self.get_logger().info("Temperature server started. Collecting every 10s...")
@@ -25,10 +25,9 @@ class TempServerNode(Node):
             self.city2_name_ = self.get_parameter("city2").value
             self.city3_name_ = self.get_parameter("city3").value
             
-            self.get_logger().info("--- Temperature Report ---")
-            self.get_logger().info("Temperature from " + self.city1_name_ + " is " + str(self.current_msg.temperature_saopaulo))
-            self.get_logger().info("Temperature from " + self.city2_name_ + " is " + str(self.current_msg.temperature_santoandre))
-            self.get_logger().info("Temperature from " + self.city3_name_ + " is " + str(self.current_msg.temperature_campinas))
+            self.get_logger().info("Temperature Collected: " + self.city1_name_ + ": " + str(self.current_msg.temperature_saopaulo) 
+                                                             + " | " + self.city2_name_ + ": " + str(self.current_msg.temperature_santoandre) 
+                                                             + " | " + self.city3_name_ + ": " + str(self.current_msg.temperature_campinas))
         else:
             self.get_logger().info("No data received yet...")
  
